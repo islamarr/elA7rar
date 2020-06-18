25fd10ff21111dfec536146b30ae7a1b60cfa55f
islam.ar <islam.ahmed@approcks.com>
2020-06-18 21:03:34 +0200

    Test

--- app/src/main/java/com/Ihsan/elAhrar/activities/SettingsActivity.kt
+++ app/src/main/java/com/Ihsan/elAhrar/activities/SettingsActivity.kt
@@ -7,7 +7,7 @@ import com.Ihsan.elAhrar.R
 
 class SettingsActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {//
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_activity)
 
