d7f4c01ed4bc84900e31f27eb3b48e97b5d92ed8
islam.ar <islam.ahmed@approcks.com>
2020-06-27 19:48:46 +0200

    Set levels

--- app/src/main/java/com/Ihsan/elAhrar/ui/adapters/HabitAdapter.kt
+++ app/src/main/java/com/Ihsan/elAhrar/ui/adapters/HabitAdapter.kt
@@ -8,6 +8,7 @@ import com.Ihsan.elAhrar.R
 import com.Ihsan.elAhrar.data.HabitViewModel
 import com.Ihsan.elAhrar.data.database.UnitHabit
 import kotlinx.android.synthetic.main.one_item_habit.view.*
+import java.util.concurrent.TimeUnit
 
 class HabitAdapter(var habitList: List<UnitHabit>, val habitViewModel: HabitViewModel) :
 
@@ -25,9 +26,16 @@ class HabitAdapter(var habitList: List<UnitHabit>, val habitViewModel: HabitView
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         holder.habitName.text = habitList[position].name
+        holder.habitTimer.text = formatCounter(habitList[position].startDate)
         holder.More.setOnClickListener {
             habitViewModel.deleteOne(habitList[position])
         }
+    }
+
+    private fun formatCounter(timeInMS: Long?): String {
+        return String.format("%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(timeInMS!!),
+            TimeUnit.MILLISECONDS.toMinutes(timeInMS) % TimeUnit.HOURS.toMinutes(1),
+            TimeUnit.MILLISECONDS.toSeconds(timeInMS) % TimeUnit.MINUTES.toSeconds(1))
     }
 
     inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
